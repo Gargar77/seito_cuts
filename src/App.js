7248779a1139b71ea666d3dcffc6e8319efffd52
gargar77 <gbautista2@knights.ucf.edu>
2021-04-30 18:09:40 -0400

    removed unneeded elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch,Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      auth:null,
+
+    }
+  }
+
+  render() {
+    return <h1>HELLOOOOOO</h1>
+  }
 }
 
 export default App;
