361cb2b747427135c30c438847a405f6111137cc
gargar77 <gbautista2@knights.ucf.edu>
2021-04-30 17:36:16 -0400

    setup firebase firestore and authentication configs

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+// the base firebase app
+import firebase from 'firebase/app'
+// the products we want to use
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyAWdrum6KrZEOdTFAlZsFfL-oaAx9Pczck",
+    authDomain: "seito-cuts.firebaseapp.com",
+    projectId: "seito-cuts",
+    storageBucket: "seito-cuts.appspot.com",
+    messagingSenderId: "384329093467",
+    appId: "1:384329093467:web:7d976c62f5da41403d0a7e",
+    measurementId: "G-6XPVPGCBLQ"
+  };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+
+    if (!snapshot.exists) {
+        const { displayName,email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch(error) {
+            console.log('error creating user',error.message)
+        }
+    }
+
+    return userRef;
+}
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+/* this parameter lets google prompt to select 
+an account everytime the provider istance is used*/
+provider.setCustomParameters({prompt:'select_account'});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+//lets also export the firebase app itself in case it needs to get used elsewhere.
+export default firebase;
