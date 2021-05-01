56b7dc843566bc7f21a00e61556c72ec536a4ba0
gargar77 <gbautista2@knights.ucf.edu>
2021-04-30 20:26:39 -0400

    added basic sign-in form layout

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 
-class SignIn extends React.Component {
-    state = {
+import './sign-in.styles.css';
 
+import FormInput from '../form-input/form-input.component';
+
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('logged in!');
+    }
+
+    handleChange = e => {
+        const { value,name } = e.target;
+        this.setState({[name]:value})
     }
 
     render() {
         return (
-            <div>Sign-in</div>
+            <div className="sign-in">
+                <h1>Sign-in</h1>
+                <form onSubmit={this.handleSubmit}>
+                     <FormInput name="email" type="email" value={this.state.email} label="Email" handleChange={this.handleChange} required/>
+                     <FormInput name="password" type="password" value={this.state.password} label="Password" handleChange={this.handleChange} required/>
+                     <button type="submit">Sign-in</button>
+                </form>
+            </div>
         )
     }
 }
