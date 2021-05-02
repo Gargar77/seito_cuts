6498beb91689ca96c5cd5b4f23d4b3af79f9a6dd
gargar77 <gbautista2@knights.ucf.edu>
2021-05-02 18:16:50 -0400

    set layout for current cut of the day display

--- src/pages/auth/homepage/homepage.component.jsx
+++ src/pages/auth/homepage/homepage.component.jsx
@@ -4,14 +4,17 @@ import './homepage.styles.css';
 
 const SAMPLE_CUTS = [
     {
+        userId:'124458',
         first: 'Gary',
         last: 'Bautista'
     },
     {
+        userId:'44839',
         first: 'Bianca',
         last: 'Dessouki'
     },
     {
+        userId:'32843',
         first: "T'mara",
         last: 'Morrow'
     }
@@ -22,8 +25,26 @@ class Homepage extends React.Component {
         currentCuts: SAMPLE_CUTS
     }
 
+    fetchCurrentCuts() {
+        // async request to firebase
+    }
+
+    componentDidMount() {
+        this.fetchCurrentCuts();
+    }
+
     render() {
-        return <h1>HomePage</h1>
+        const {currentCuts} = this.state;
+        return (
+            <div className="homepage">
+                <h1>Current Cuts</h1>
+                <ol className="cut-view">
+                    {currentCuts.map((cutInfo,idx)=> {
+                        return <li key={idx}>{`${cutInfo.first} ${cutInfo.last.slice(0,1)}`}</li> 
+                    })}
+                </ol>
+            </div>
+        )
     }
 }
 
